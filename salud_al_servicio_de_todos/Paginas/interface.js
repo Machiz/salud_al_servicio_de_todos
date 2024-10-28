0ec6a94c5681a6af87132b9c94a19e434be8535d
Marcelo Hernandez <marcelo.hernren@hotmail.com>
2024-10-28 13:30:47 -0500

    cambios en la interfaz... en proceso...

---
+++ salud_al_servicio_de_todos/Paginas/interface.js
@@ -0,0 +1,22 @@
+const input_range = document.querySelector("#search_value_range");
+const lbl_size = document.querySelector("#search_range_lbl");
+//const lbl_size_conexion_ramge = document.querySelector("#value_conexion");
+//const matrix_form = document.querySelector("#matrix_form");
+//const btn_random = document.querySelector("#btn_random_matrix");
+//const conexion_range = document.querySelector("#range_conexion");
+function updateMatrix() {
+    //const size = getSizeMatrix();
+    lbl_size.innerText = `rango de busqueda: ${size} km`;
+    updateConexionRange();
+    //generateMatrix(size);
+    //getMatrix(size);
+}
+function getConexionRange() {
+    return parseInt(conexion_range.value);
+}
+  
+  // función para obtener la celda de la matriz
+  function updateConexionRange() {
+    const value = getConexionRange();
+    lbl_size_conexion_ramge.innerText = `Conexion: ${value}%`;
+  }

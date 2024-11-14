af3806158f5909e6feda0c6b63946638a6c2d35b
Moroff3 <Crostzard@outlook.com>
2024-11-14 08:31:40 -0500

    Optimización de nodos y aristas

--- salud_al_servicio_de_todos/test_folium.py
+++ salud_al_servicio_de_todos/test_folium.py
@@ -1,8 +1,6 @@
 import folium
 import pandas as pd
-import IPython as ip
-import networkx as nx
-from pyvis.network import Network
+import time
 import numpy as np
 from math import radians, sin, cos, sqrt, atan2
 
@@ -23,41 +21,87 @@ f = folium.Figure(width=1000, height=500)
 m.add_to(f) # agregamos el mapa m a la figura f para mantenerla en un recuadro creo
 
 def haversine(lat1, lon1, lat2, lon2):
-    R = 6371.0
     dlat = radians(lat2 - lat1)
     dlon = radians(lon2 - lon1)
     a = sin(dlat / 2)**2 + cos(radians(lat1)) * cos(radians(lat2)) * sin(dlon / 2)**2
     c = 2 * atan2(sqrt(a), sqrt(1 - a))
-    distancia = R * c
+    distancia = 6371.0 * c
     return round(distancia, 2) # Distancia aproximada a dos decimales
 
+def create_Dataframe():
+    df = pd.read_csv("data\out.csv", encoding='utf8', sep = ',') # Lectura del .csv, datos separados por coma
+    return df
 
-def optimizado_dibujar(num_nodos, cantidad, dibujar_aristas=False):
-  df = pd.read_csv("data\out.csv", encoding='utf8', sep = ',', keep_default_na=False) # Lectura del .csv, datos separados por coma
-  df = df.drop_duplicates(subset = 'nombre')
+def apply_dibujar(row,df):
+  la = row['latitud']
+  lo = row['longitud']
+  ca = row['categoria']
+  no = row['nombre']
 
-  for i in df.sample(n=cantidad).index:
+  folium.Circle(
+        location=[la, lo],
+        radius=150,
+        fill_opacity=1,
+        fill_color="lightblue",
+        popup= no + " -" + ca,
+  ).add_to(m)
+
+  for j in range(len(df)):
+      if row["id_eess"] != df["id_eess"].iloc[j]: 
+            la1 = np.float32(la)
+            lo1 = np.float32(lo)
+            la2 = np.float32(df['latitud'].iloc[j])
+            lo2 = np.float32(df['longitud'].iloc[j])
+            distancia = haversine(la1, lo1, la2, lo2)
+            if distancia <= 20:
+                folium.PolyLine(
+                    locations=[
+                    [la1, lo1], 
+                    [la2, lo2],],
+                    color="red",
+                    weight=1,
+                    popup= distancia,
+                ).add_to(m)
+    
+
+def optimizado_dibujar(df, dibujar_aristas=False):
+  cantidad = len(df.index)
+  for i in range(cantidad):  
      la = df['latitud'].iloc[i]
-     if(la == ''): continue 
      lo = df['longitud'].iloc[i]
      ca = df['categoria'].iloc[i]
      no = df['nombre'].iloc[i]
 
      folium.Circle(
-            location=[la, lo],
-            radius=150,
-            fill_opacity=1,
-            fill_color="lightblue",
-            popup= no + " -" + ca,
+        location=[la, lo],
+        radius=150,
+        fill_opacity=1,
+        fill_color="lightblue",
+        popup= no + " -" + ca,
      ).add_to(m)
-     
-def dibujar_grafo(num_nodos, cantidad, dibujar_aristas=False):
+  
+  if not dibujar_aristas: return
+  for i in range(cantidad):
+    for j in range(cantidad):
+      if df["id_eess"].iloc[i] != df["id_eess"].iloc[j]: 
+            la1 = np.float32(df['latitud'].iloc[i])
+            lo1 = np.float32(df['longitud'].iloc[i])
+            la2 = np.float32(df['latitud'].iloc[j])
+            lo2 = np.float32(df['longitud'].iloc[j])
+            distancia = haversine(la1, lo1, la2, lo2)
+            if distancia <= 20:
+                folium.PolyLine(
+                    locations=[
+                    [la1, lo1], 
+                    [la2, lo2],],
+                    color="red",
+                    weight=1,
+                    popup= distancia,
+                        
+                ).add_to(m)
+        
+def dibujar_grafo(df, dibujar_aristas=False):
   # se usa keep_default_na para que los valores vacios nos den un string vacio "" en vez de un "Nan"
-  df = pd.read_csv("data\out.csv", encoding='utf8', sep = ',', keep_default_na=False) # Lectura del .csv, datos separados por coma
-  
-  df = df.drop_duplicates(subset = 'nombre')
-  
-  df = df.sample(n=cantidad)
   nodos = []
 
   for index, row in df.iterrows():
@@ -102,11 +146,16 @@ def dibujar_grafo(num_nodos, cantidad, dibujar_aristas=False):
                         popup= distancia,
                         
                     ).add_to(m)
-        
 
 csv_size = 16368 # cantidad de datos aproximado en el csv
-cantidad = 1000 # maxima cantidad de Circles parece ser de 2060, por qué? no lo sé, maxima cantidad de Circle Markers?
-dibujar_grafo(csv_size, cantidad, False)
-# optimizado_dibujar(csv_size,cantidad, False)
+cantidad = 100 # maxima cantidad de Circles parece ser de 2060, por qué? no lo sé, maxima cantidad de Circle Markers?
+# dibujar_grafo(csv_size, cantidad, False)
 
+df = create_Dataframe()
+df = df.sample(n=cantidad)
+t = time.time()
+# dibujar_grafo(df, False)
+# optimizado_dibujar(df, True)
+df.apply(apply_dibujar, axis=1, args=(df,))
+print((time.time() - t) * 1000, "ms")
 m.save('templates/folium_map.html')

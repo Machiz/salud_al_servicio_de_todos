3c641990181b9666055298badcc6954ccc88f8e2
Moroff3 <Crostzard@outlook.com>
2024-11-16 17:36:49 -0500

    Networkx a Folium

--- salud_al_servicio_de_todos/test_folium.py
+++ salud_al_servicio_de_todos/test_folium.py
@@ -3,7 +3,7 @@ import folium.map
 import pandas as pd
 import time
 import numpy as np
-import math
+import networkx as nx
 import heapq as hp
 from math import radians, sin, cos, sqrt, atan2
 
@@ -82,123 +82,90 @@ def apply_dibujar(row, df, aristas = False):
   no = row['nombre']
 
   folium.Circle(
-        location=[la, lo],
-        radius=150,
-        fill_opacity=1,
-        fill_color="lightblue",
-        popup= no + " -" + ca,
+    location=[la, lo],
+    radius=150,
+    fill_opacity=1,
+    fill_color="lightblue",
+    popup= no + " -" + ca,
   ).add_to(m)
 
   if(aristas == False): return
   for j in range(len(df)):
-      if row["id_eess"] != df["id_eess"].iloc[j]: 
-            la1 = np.float32(la)
-            lo1 = np.float32(lo)
-            la2 = np.float32(df['latitud'].iloc[j])
-            lo2 = np.float32(df['longitud'].iloc[j])
-            distancia = haversine(la1, lo1, la2, lo2)
-            if distancia <= 20:
-                folium.PolyLine(
-                    locations=[
-                    [la1, lo1], 
-                    [la2, lo2],],
-                    color="red",
-                    weight=1,
-                    popup= distancia,
-                ).add_to(m)
+    if row["id_eess"] != df["id_eess"].iloc[j]: 
+      la1 = np.float32(la)
+      lo1 = np.float32(lo)
+      la2 = np.float32(df['latitud'].iloc[j])
+      lo2 = np.float32(df['longitud'].iloc[j])
+      distancia = haversine(la1, lo1, la2, lo2)
+      if distancia <= 20:
+          folium.PolyLine(
+              locations=[
+              [la1, lo1], 
+              [la2, lo2],],
+              color="red",
+              weight=1,
+              popup= distancia,
+          ).add_to(m)
 
-def optimizado_dibujar(df, dibujar_aristas=False):
-  cantidad = len(df.index)
-  for i in range(cantidad):  
-     la = df['latitud'].iloc[i]
-     lo = df['longitud'].iloc[i]
-     ca = df['categoria'].iloc[i]
-     no = df['nombre'].iloc[i]
+def create_networkx(row, df, graph):
+  graph.add_node(row['nombre'])
+  la = row['latitud']
+  lo = row['longitud']
+  for j in range(len(df)):
+    if row["id_eess"] != df["id_eess"].iloc[j]: 
+      la1 = np.float32(la)
+      lo1 = np.float32(lo)
+      la2 = np.float32(df['latitud'].iloc[j])
+      lo2 = np.float32(df['longitud'].iloc[j])
+      distancia = haversine(la1, lo1, la2, lo2)
+      if distancia <= 20:
+        graph.add_edge(row['nombre'], df['nombre'].iloc[j], weight= distancia)  
 
-     folium.Circle(
-        location=[la, lo],
-        radius=150,
-        fill_opacity=1,
-        fill_color="lightblue",
-        popup= no + " -" + ca,
-     ).add_to(m)
-  
-  if not dibujar_aristas: return
-  for i in range(cantidad):
-    for j in range(cantidad):
-      if df["id_eess"].iloc[i] != df["id_eess"].iloc[j]: 
-            la1 = np.float32(df['latitud'].iloc[i])
-            lo1 = np.float32(df['longitud'].iloc[i])
-            la2 = np.float32(df['latitud'].iloc[j])
-            lo2 = np.float32(df['longitud'].iloc[j])
-            distancia = haversine(la1, lo1, la2, lo2)
-            if distancia <= 20:
-                folium.PolyLine(
-                    locations=[
-                    [la1, lo1], 
-                    [la2, lo2],],
-                    color="red",
-                    weight=1,
-                    popup= distancia,
-                        
-                ).add_to(m)
+def folium_from_nx(row, df, graph, mapa):
+  for i in list(graph.nodes):
+    if(i != row['nombre']): continue
+    la = row['latitud']
+    lo = row['longitud']
+    ca = row['categoria']
+    no = row['nombre']
 
-def dibujar_grafo(df, dibujar_aristas=False):
-  # se usa keep_default_na para que los valores vacios nos den un string vacio "" en vez de un "Nan"
-  nodos = []
-
-  for index, row in df.iterrows():
-
-    # if("LIMA" not in row['diresa']): continue  # Tomamos los departamentos que tengan LIMA en el nombre
-    if(row['latitud'] == "" or row['longitud'] == ""): continue # Algunos tienen la longitud o latitud vacios, no los queremos ver.
-    nombre = row['nombre']
-    # Creamos un circulo de Folium que representa a un nodo.
     folium.Circle(
-        location=[row['latitud'], row['longitud']],
-        radius=150,
-        fill_opacity=1,
-        fill_color="lightblue",
-        popup= nombre + " -" + row['categoria'],
-    ).add_to(m)
-    nodos.append(row)
+      location=[la, lo],
+      radius=150,
+      fill_opacity=1,
+      fill_color="lightblue",
+      popup= no + " -" + ca,
+    ).add_to(mapa)
 
-  
-  if dibujar_aristas:
-    max_ar = 4
-    can_ar = 0
-    for row1 in nodos:
-        can_ar = 0
-        for row2 in nodos:
-            
-            if row1["id_eess"] != row2["id_eess"]:
-                
-                la1 = np.float32(row1['latitud'])
-                lo1 = np.float32(row1['longitud'])
-                la2 = np.float32(row2['latitud'])
-                lo2 = np.float32(row2['longitud'])
-                distancia = haversine(la1, lo1, la2, lo2)
-                if distancia <= 20:
-                    can_ar += 1
-                    folium.PolyLine(
-                        locations=[
-                        [la1, lo1], 
-                        [la2, lo2],
-                        ],
-                        color="red",
-                        weight=1,
-                        popup= distancia,
-                        
-                    ).add_to(m)
+    for j in range(len(df)):
+      if row["id_eess"] != df["id_eess"].iloc[j]: 
+        la1 = np.float32(la)
+        lo1 = np.float32(lo)
+        la2 = np.float32(df['latitud'].iloc[j])
+        lo2 = np.float32(df['longitud'].iloc[j])
+        distancia = haversine(la1, lo1, la2, lo2)
+        if distancia <= 20:
+          folium.PolyLine(
+              locations=[
+              [la1, lo1], 
+              [la2, lo2],],
+              color="red",
+              weight=1,
+              popup= distancia,
+          ).add_to(mapa)
 
 csv_size = 16368 # cantidad de datos aproximado en el csv
 cantidad = 100 # maxima cantidad de Circles parece ser de 2060, por qué? no lo sé, maxima cantidad de Circle Markers?
 # dibujar_grafo(csv_size, cantidad, False)
 
-df = create_Dataframe()
-df = df.sample(n=cantidad)
+df = create_Dataframe()    # creamos dataframe
+df = df.sample(n=cantidad) # elegir datos dispersos del dataframe
+
+graph = nx.Graph()
+
+df.apply(create_networkx, axis=1, args=(df, graph,)) # creamos grafo networkx
 t = time.time()
-# dibujar_grafo(df, False)
-# optimizado_dibujar(df, True)
-df.apply(apply_dibujar, axis=1, args=(df,False))
+df.apply(folium_from_nx, axis=1, args=(df, graph, m))
+# df.apply(apply_dibujar, axis=1, args=(df,False))
 print((time.time() - t) * 1000, "ms")
 m.save('templates/folium_map.html')

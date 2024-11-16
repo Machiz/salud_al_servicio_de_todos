458301e48d7455b3409ed9049eaba191dea3a613
Moroff3 <Crostzard@outlook.com>
2024-11-16 16:06:01 -0500

    Filtrar en folium

--- salud_al_servicio_de_todos/test_folium.py
+++ salud_al_servicio_de_todos/test_folium.py
@@ -1,7 +1,10 @@
 import folium
+import folium.map
 import pandas as pd
 import time
 import numpy as np
+import math
+import heapq as hp
 from math import radians, sin, cos, sqrt, atan2
 
 
@@ -32,7 +35,16 @@ def create_Dataframe():
     df = pd.read_csv("data\out.csv", encoding='utf8', sep = ',') # Lectura del .csv, datos separados por coma
     return df
 
-def apply_dibujar(row,df):
+def filtrar(departamento):
+  mapa = folium.Map([-8.35, -74.6972], zoom_start=6, tiles= "CartoDB.Positron", min_zoom = 5, max_zoom=15,  max_bounds=True,
+    min_lat=min_lat,max_lat=max_lat,
+    min_lon=min_lon,max_lon=max_lon,)
+  df.apply(apply_departamento, axis=1, args=(df,departamento, mapa))
+  mapa.save('templates/folium_map.html')
+
+def apply_departamento(row, df, departamento, mapa):
+  if(departamento not in row['diresa']): return
+
   la = row['latitud']
   lo = row['longitud']
   ca = row['categoria']
@@ -44,7 +56,7 @@ def apply_dibujar(row,df):
         fill_opacity=1,
         fill_color="lightblue",
         popup= no + " -" + ca,
-  ).add_to(m)
+  ).add_to(mapa)
 
   for j in range(len(df)):
       if row["id_eess"] != df["id_eess"].iloc[j]: 
@@ -61,8 +73,39 @@ def apply_dibujar(row,df):
                     color="red",
                     weight=1,
                     popup= distancia,
+                ).add_to(mapa)
+
+def apply_dibujar(row, df, aristas = False):
+  la = row['latitud']
+  lo = row['longitud']
+  ca = row['categoria']
+  no = row['nombre']
+
+  folium.Circle(
+        location=[la, lo],
+        radius=150,
+        fill_opacity=1,
+        fill_color="lightblue",
+        popup= no + " -" + ca,
+  ).add_to(m)
+
+  if(aristas == False): return
+  for j in range(len(df)):
+      if row["id_eess"] != df["id_eess"].iloc[j]: 
+            la1 = np.float32(la)
+            lo1 = np.float32(lo)
+            la2 = np.float32(df['latitud'].iloc[j])
+            lo2 = np.float32(df['longitud'].iloc[j])
+            distancia = haversine(la1, lo1, la2, lo2)
+            if distancia <= 20:
+                folium.PolyLine(
+                    locations=[
+                    [la1, lo1], 
+                    [la2, lo2],],
+                    color="red",
+                    weight=1,
+                    popup= distancia,
                 ).add_to(m)
-    
 
 def optimizado_dibujar(df, dibujar_aristas=False):
   cantidad = len(df.index)
@@ -99,7 +142,7 @@ def optimizado_dibujar(df, dibujar_aristas=False):
                     popup= distancia,
                         
                 ).add_to(m)
-        
+
 def dibujar_grafo(df, dibujar_aristas=False):
   # se usa keep_default_na para que los valores vacios nos den un string vacio "" en vez de un "Nan"
   nodos = []
@@ -156,6 +199,6 @@ df = df.sample(n=cantidad)
 t = time.time()
 # dibujar_grafo(df, False)
 # optimizado_dibujar(df, True)
-df.apply(apply_dibujar, axis=1, args=(df,))
+df.apply(apply_dibujar, axis=1, args=(df,False))
 print((time.time() - t) * 1000, "ms")
 m.save('templates/folium_map.html')
